cd9b5c04fa6771462f7e172589a5125e63249b95
Garret-Tade <132246752+Garret-Tade@users.noreply.github.com>
2024-12-16 17:07:57 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
+from flask import Flask
 
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hello, Heroku! The scheduling app is live!"
+
+if __name__ == "__main__":
+    app.run()
